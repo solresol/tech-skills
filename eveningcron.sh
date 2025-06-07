535a0a19b870e990a7d92f68d40eac43857ea989
Greg Baker <gregb@ifost.org.au>
2025-06-08 07:25:36 +1000

    Pointless. It would always return nothing because we processed everything in the previous line.

--- eveningcron.sh
+++ eveningcron.sh
@@ -4,7 +4,6 @@ cd $(dirname $0)
 
 uv run batchfetch.py --show-costs
 uv run process_director_compensation.py --show-costs
-uv run compensation_batch_check.py
 uv run fetch_prices_for_director_filings.py --stop-after 200
 uv run board_stock_analysis.py
 uv run boards_website_generator.py
