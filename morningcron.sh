a6ce95abf6b6d8394d482f51637c867b753e21f7
Greg Baker <gregb@ifost.org.au>
2025-06-16 16:18:50 +1000

    Save myself a little bit of effort.

--- morningcron.sh
+++ morningcron.sh
@@ -2,6 +2,20 @@
 
 cd $(dirname $0)
 
+# We might as well use the latest version
+git pull
+
+# This is the core of it
 uv run ask_openai_bulk.py --stop-after 100
+
+# I'm not sure if this is working
 uv run extract_director_compensation.py --stop-after 110
+
+# Because sector information fails pretty regularly, I often 
+# ask codex to populate the mistakes manually. Why I don't
+# just get it to do everything is a mystery that past-me is
+# holding back on
+psql -f manual_sector_inserts.sql
+
+# This fails pretty regularly for a lot of sectors
 uv run fetch_all_sectors.py --stop-after 500
